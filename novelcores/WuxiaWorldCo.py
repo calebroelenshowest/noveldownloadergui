01cd4ca28666a245fc98b7b30ebb21b856232b28
calebroelenshowest <55787034+calebroelenshowest@users.noreply.github.com>
2020-10-28 17:45:43 +0100

    WuxiaWorldCo.py chapter + image added

--- novelcores/WuxiaWorldCo.py
+++ novelcores/WuxiaWorldCo.py
@@ -30,8 +30,22 @@ class WuxiaWorldCo:
 
     @staticmethod
     def get_url_chapters(soup: BeautifulSoup) -> list:
-        pass
+        chapters_item = soup.find_all("a", class_="chapter-item")
+        chapters = []
+        for chapter in chapters_item:
+            title = chapter.find("p", class_="chapter-name").text
+            chapter_a = BeautifulSoup(str(chapter), 'html.parser').find("a")["href"]
+            template = {"url": "https://www.wuxiaworld.co" + chapter_a, "title": title}
+            chapters.append(template)
+        return chapters
 
     @staticmethod
     def get_url_image(soup: BeautifulSoup) -> str:
-        pass
+        img = soup.find("div", class_="book-img").find("img")["src"]
+        return img
+
+
+if __name__ == "__main__":
+    s = WuxiaWorldCo.get_soup("https://www.wuxiaworld.co/Genius-Doctor--Black-Belly-Miss/")
+    img = WuxiaWorldCo.get_url_image(s)
+    chap = WuxiaWorldCo.get_url_chapters(s)
